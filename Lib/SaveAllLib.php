515078a0e526c0ab28cfb07565b0c3e013305d70
kimsia <kimcity@gmail.com>
2011-12-02 11:26:58 +0800

    add in a new Lib for checking if a saveAll non-atomic result was unsuccessful

---
+++ Lib/SaveAllLib.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * SaveAll Utility Library
+ *
+ * This Utility Library is for SaveAll methods
+ * http://github.com/simkimsia/UtilLib
+ *
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2011, Sim Kim Sia
+ * @link http://simkimsia.com
+ * @author Sim Kim Sia (kimcity@gmail.com)
+ * @package app
+ * @subpackage app.Lib
+ * @filesource
+ * @version 0.1
+ * @lastmodified 2011-12-02 
+ */
+class SaveAllLib {
+	
+	/**
+	* 
+	* Returns true if a saveAll non-atomic transaction is successful. Returns false if otherwise.
+	*
+	* @param array $result
+	* @return array
+	**/
+	public static function hasASuccessful($result) {
+		
+		if (is_array($result)) {
+			foreach($result as $key=>$value) {
+				if ($value === false) {
+					return false;
+				}
+			}
+		   	return true;
+		} elseif (is_boolean($result)) {
+			return $result;
+		}
+		
+		return false;
+	}
+}
+?>

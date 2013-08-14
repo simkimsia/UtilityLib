197a64d41738f756d397059963b02a4c71f8a6af
kimsia <kimcity@gmail.com>
2013-08-14 16:36:53 +0800

    convert objects to array and vice versa

--- Lib/ArrayLib.php
+++ Lib/ArrayLib.php
@@ -95,4 +95,56 @@ class ArrayLib {
 		return $extractedData;
 	}
 
+/**
+ * Convert stdClass objects to multi-dimensional arrays
+ *
+ * @author JR
+ * @link http://www.if-not-true-then-false.com/2009/php-tip-convert-stdclass-object-to-multidimensional-array-and-convert-multidimensional-array-to-stdclass-object/
+ * @param mixed $d
+ * @return Array
+ */
+	public static function objectToArray($d) {
+		if (is_object($d)) {
+			// Gets the properties of the given object
+			// with get_object_vars function
+			$d = get_object_vars($d);
+		}
+ 
+		if (is_array($d)) {
+			/*
+			* Return array converted to object
+			* Using __FUNCTION__ (Magic constant)
+			* for recursive call
+			*/
+			return array_map(self::__FUNCTION__, $d);
+		}
+		else {
+			// Return array
+			return $d;
+		}
+	}
+
+/**
+ * Convert multi-dimensional arrays to stdClass objects
+ *
+ * @author JR
+ * @link http://www.if-not-true-then-false.com/2009/php-tip-convert-stdclass-object-to-multidimensional-array-and-convert-multidimensional-array-to-stdclass-object/
+ * @param mixed $d
+ * @return object
+ */
+	public static function arrayToObject($d) {
+		if (is_array($d)) {
+			/*
+			* Return array converted to object
+			* Using __FUNCTION__ (Magic constant)
+			* for recursive call
+			*/
+			return (object) array_map(self::__FUNCTION__, $d);
+		}
+		else {
+			// Return object
+			return $d;
+		}
+	}
+
 }

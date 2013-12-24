a8080d225991a811e904b1038fd068101300ec55
AikChun <aikchun616@gmail.com>
2013-12-23 19:19:05 -0800

    added unzip function

--- Lib/ZipLib.php
+++ Lib/ZipLib.php
@@ -65,47 +65,93 @@ class ZipLib {
 			unlink ($destination);
 		}
 
-		$zip = new ZipArchive();
-		if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
-			return false;
-		}
-
-		if (is_dir($source) === true) {
-			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
-
-			if ($include_dir) {
-				$arr = explode("/",$source);
-				$maindir = $arr[count($arr)- 1];
-				$source = "";
-				for ($i=0; $i < count($arr) - 1; $i++) { 
-					$source .= '/' . $arr[$i];
-				}
-				$source = substr($source, 1);
-				$zip->addEmptyDir($maindir);
-			}
-
-			foreach ($files as $file) {
-				$file = str_replace('\\', '/', $file);
-
-				// purposely ignore files that are irrelevant
-				if( in_array(substr($file, strrpos($file, '/')+1), $ignoreFiles) )
-					continue;
-
-				$file = realpath($file);
-
-				if (is_dir($file) === true)
-				{
-					$zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-				}
-				else if (is_file($file) === true)
-				{
-					$zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
-				}
-			}
-		} else if (is_file($source) === true) {
-			$zip->addFromString(basename($source), file_get_contents($source));
-		}
-
-		return $zip->close();
 	}
+
+
+	/**
+	 * Unzip the source_file in the destination dir
+	 *
+	 * @param   string      The path to the ZIP-file.
+	 * @param   string      The path where the zipfile should be unpacked, if false the directory of the zip-file is used
+	 * @param   boolean     Indicates if the files will be unpacked in a directory with the name of the zip-file (true) or not (false) (only if the destination directory is set to false!)
+	 * @param   boolean     Overwrite existing files (true) or not (false)
+	 *  
+	 * @return  boolean     Succesful or not
+	 */
+	public static function unzip($src_file, $dest_dir=false, $create_zip_name_dir=true, $overwrite=true) {
+		$zip = zip_open($src_file); 
+		if ($zip) {
+			$splitter = ($create_zip_name_dir === true) ? "." : "/";
+			if ($dest_dir === false) {
+				$dest_dir = substr($src_file, 0, strrpos($src_file, $splitter))."/";
+			} 
+			// Create the directories to the destination dir if they don't already exist
+			self::create_dirs($dest_dir);
+
+			// For every file in the zip-packet
+			while ($zip_entry = zip_read($zip)) {
+			// Now we're going to create the directories in the destination directories
+			
+			// If the file is not in the root dir
+			$pos_last_slash = strrpos(zip_entry_name($zip_entry), "/");
+			if ($pos_last_slash !== false)
+			{
+				// Create the directory where the zip-entry should be saved (with a "/" at the end)
+				self::create_dirs($dest_dir.substr(zip_entry_name($zip_entry), 0, $pos_last_slash+1));
+			}
+
+			// Open the entry
+			if (zip_entry_open($zip,$zip_entry,"r")) {
+				
+				// The name of the file to save on the disk
+				$file_name = $dest_dir.zip_entry_name($zip_entry);
+				
+				// Check if the files should be overwritten or not
+				if ($overwrite === true || $overwrite === false && !is_file($file_name)) {
+				// Get the content of the zip entry
+				$fstream = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+				file_put_contents($file_name, $fstream );
+				// Set the rights
+				chmod($file_name, 0777);
+				echo "save: ".$file_name."<br />";
+				}
+				
+				// Close the entry
+				zip_entry_close($zip_entry);
+			}
+			}
+			// Close the zip-file
+			zip_close($zip);
+		}
+		 
+		return true;
+	}
+
+/**
+ * This function creates recursive directories if it doesn't already exist
+ *
+ * @param String The path that should be created
+ *
+ * @return  void
+ */
+	public static function create_dirs($path) {
+		if (!is_dir($path)) {
+		$directory_path = "";
+		$directories = explode("/",$path);
+		array_pop($directories);
+		
+			foreach($directories as $directory) {
+				$directory_path .= $directory."/";
+				if (!is_dir($directory_path)) {
+					mkdir($directory_path);
+					chmod($directory_path, 0777);
+				} // end if !is_dir
+
+			} // end foreach 
+
+		} // end if !is_dir
+	}
+
+
 }

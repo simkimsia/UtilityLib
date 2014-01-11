9611721eb6ff770c0d57216e318a91184a8bc084
kimsia <kimcity@gmail.com>
2014-01-11 23:21:14 +0800

    refactor spaces for ZipLib

--- Lib/ZipLib.php
+++ Lib/ZipLib.php
@@ -23,9 +23,9 @@
 class ZipLib {
 
 /**
- * 
+ *
  * Given a path of a folder, zip folder recursively to produce a zip file.
- * Automatically ignores ".", "..", and ".DS_Store". 
+ * Automatically ignores ".", "..", and ".DS_Store".
  *
  * #### Options
  *
@@ -75,56 +75,56 @@ class ZipLib {
 	 * @param   string      The path where the zipfile should be unpacked, if false the directory of the zip-file is used
 	 * @param   boolean     Indicates if the files will be unpacked in a directory with the name of the zip-file (true) or not (false) (only if the destination directory is set to false!)
 	 * @param   boolean     Overwrite existing files (true) or not (false)
-	 *  
+	 *
 	 * @return  boolean     Succesful or not
 	 */
 	public static function unzip($src_file, $dest_dir=false, $create_zip_name_dir=true, $overwrite=true) {
-		$zip = zip_open($src_file); 
+		$zip = zip_open($src_file);
 		if ($zip) {
 			$splitter = ($create_zip_name_dir === true) ? "." : "/";
 			if ($dest_dir === false) {
 				$dest_dir = substr($src_file, 0, strrpos($src_file, $splitter))."/";
-			} 
+			}
 			// Create the directories to the destination dir if they don't already exist
 			self::create_dirs($dest_dir);
 
 			// For every file in the zip-packet
 			while ($zip_entry = zip_read($zip)) {
-			// Now we're going to create the directories in the destination directories
-			
-			// If the file is not in the root dir
-			$pos_last_slash = strrpos(zip_entry_name($zip_entry), "/");
-			if ($pos_last_slash !== false)
-			{
-				// Create the directory where the zip-entry should be saved (with a "/" at the end)
-				self::create_dirs($dest_dir.substr(zip_entry_name($zip_entry), 0, $pos_last_slash+1));
-			}
+				// Now we're going to create the directories in the destination directories
 
-			// Open the entry
-			if (zip_entry_open($zip,$zip_entry,"r")) {
-				
-				// The name of the file to save on the disk
-				$file_name = $dest_dir.zip_entry_name($zip_entry);
-				
-				// Check if the files should be overwritten or not
-				if ($overwrite === true || $overwrite === false && !is_file($file_name)) {
-				// Get the content of the zip entry
-				$fstream = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-
-				file_put_contents($file_name, $fstream );
-				// Set the rights
-				chmod($file_name, 0777);
-				//echo "save: ".$file_name."<br />";
+				// If the file is not in the root dir
+				$pos_last_slash = strrpos(zip_entry_name($zip_entry), "/");
+				if ($pos_last_slash !== false)
+				{
+					// Create the directory where the zip-entry should be saved (with a "/" at the end)
+					self::create_dirs($dest_dir.substr(zip_entry_name($zip_entry), 0, $pos_last_slash+1));
 				}
-				
-				// Close the entry
-				zip_entry_close($zip_entry);
-			}
+
+				// Open the entry
+				if (zip_entry_open($zip,$zip_entry,"r")) {
+
+					// The name of the file to save on the disk
+					$file_name = $dest_dir.zip_entry_name($zip_entry);
+
+					// Check if the files should be overwritten or not
+					if ($overwrite === true || $overwrite === false && !is_file($file_name)) {
+					// Get the content of the zip entry
+					$fstream = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+					file_put_contents($file_name, $fstream );
+					// Set the rights
+					chmod($file_name, 0777);
+					//echo "save: ".$file_name."<br />";
+					}
+
+					// Close the entry
+					zip_entry_close($zip_entry);
+				}
 			}
 			// Close the zip-file
 			zip_close($zip);
 		}
-		 
+
 		return true;
 	}
 
@@ -140,7 +140,7 @@ class ZipLib {
 		$directory_path = "";
 		$directories = explode("/",$path);
 		array_pop($directories);
-		
+
 			foreach($directories as $directory) {
 				$directory_path .= $directory."/";
 				if (!is_dir($directory_path)) {
@@ -148,7 +148,7 @@ class ZipLib {
 					chmod($directory_path, 0777);
 				} // end if !is_dir
 
-			} // end foreach 
+			} // end foreach
 
 		} // end if !is_dir
 	}
